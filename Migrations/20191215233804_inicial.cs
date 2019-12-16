0e44ff2d09ba19fd4be0a816a929d362ebfa15a5
Adauto <dadorpm@hotmail.com>
2019-12-15 23:11:48 -0300

    Conclusao do projeto

---
+++ Migrations/20191215233804_inicial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Loja.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roupa",
+                columns: table => new
+                {
+                    RoupaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roupa", x => x.RoupaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    PedidoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoupaId = table.Column<int>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.PedidoId);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Roupa_RoupaId",
+                        column: x => x.RoupaId,
+                        principalTable: "Roupa",
+                        principalColumn: "RoupaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_RoupaId",
+                table: "Pedido",
+                column: "RoupaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Roupa");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Loja.Data;
 
 namespace Loja
 {
@@ -24,6 +26,9 @@ namespace Loja
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LojaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LojaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
